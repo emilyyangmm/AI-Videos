4e7b43c94c54d050bdce92884ba12a1caf7a741b
emilyyangmm <239927937@qq.com>
2026-03-26 09:16:33 +0000

    fix: 修复跨域配置

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   // outputFileTracingRoot: path.resolve(__dirname, '../../'),  // Uncomment and add 'import path from "path"' if needed
   /* config options here */
-  allowedDevOrigins: ['*.dev.coze.site'],
+  allowedDevOrigins: ['*.dev.coze.site', '130.211.240.194'],
   images: {
     remotePatterns: [
       {
